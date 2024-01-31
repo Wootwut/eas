9c7b04b0e5b9218179d0ecb7273fe11f71153cba
Bernard Madureira <88686573+Wootwut@users.noreply.github.com>
2024-01-31 18:02:45 +0100

    Create a prompt and a button The button activates the prompt for a user choice. I have to figure out how to reset the grid when the user makes the choice.

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 const container = document.querySelector(".container")
+const buttonGrid = document.querySelector(".grid")
+buttonGrid.addEventListener('click', function(){
+    let choice = window.prompt("Choose a number (max 100)")
+    if (choice > 100){
+        alert("Under 100!");
+        choice = window.prompt("Choose a number (max 100)")
+    }
+    else {
+        grid(choice, choice);
+    }
+})
 
+let row = choice;
+let column = choice;
 function grid(row,column){
+    const gridSquareWidth = 100 / column ;
+    const gridSquareHeight = 100 / row ;
+   
     for (let i=0; i<row; i++){
        for(let j=0; j<column; j++){
         const gridSquare = document.createElement('div');
         gridSquare.classList.add("grid-square");
+        gridSquare.style.width = `${gridSquareWidth}%`;
+        gridSquare.style.height = `${gridSquareHeight}%`;
+        gridSquare.addEventListener('mouseenter',function(){
+            gridSquare.classList.add('black-hover')
+        })
         container.appendChild(gridSquare)
 
        }
     }
 }
-grid (16,16)
+grid (32,32)
