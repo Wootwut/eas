5c89582c31110926eb7e6e1c766d4b7718f4a116
Bernard Madureira <88686573+Wootwut@users.noreply.github.com>
2024-02-01 07:20:48 +0100

    The basic function works I don't really know what changed, it just started working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container")
 const buttonGrid = document.querySelector(".grid")
 buttonGrid.addEventListener('click', function(){
     let choice = window.prompt("Choose a number (max 100)")
-    if (choice > 100){
+    if (choice > 100 ){
         alert("Under 100!");
         choice = window.prompt("Choose a number (max 100)")
     }
@@ -10,19 +10,21 @@ buttonGrid.addEventListener('click', function(){
         grid(choice, choice);
     }
 })
-
-let row = choice;
-let column = choice;
+function clearGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild)
+    }
+}
 function grid(row,column){
-    const gridSquareWidth = 100 / column ;
-    const gridSquareHeight = 100 / row ;
+    
    
+   clearGrid()
     for (let i=0; i<row; i++){
        for(let j=0; j<column; j++){
         const gridSquare = document.createElement('div');
         gridSquare.classList.add("grid-square");
-        gridSquare.style.width = `${gridSquareWidth}%`;
-        gridSquare.style.height = `${gridSquareHeight}%`;
+        gridSquare.style.width = `${640 / column }px`;
+        gridSquare.style.height = `${640 / row }px`;
         gridSquare.addEventListener('mouseenter',function(){
             gridSquare.classList.add('black-hover')
         })
@@ -31,4 +33,4 @@ function grid(row,column){
        }
     }
 }
-grid (32,32)
+grid (12,12)
